fe12beb10860709d7ce486bef27ac3ce6f2115af
angelialau <angelialkm@gmail.com>
2020-07-17 16:26:13 -0400

    Plot 10 players using imshow

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+import plotly.express as px
+import pandas as pd
 
 ########### Set up the chart
+# load data
+METRICS = ['Game Sense', 'Jumping', 'Power', 'Speed', 'Stamina', 'Technique']
+data = pd.read_csv('haikyuu_players.csv', index_col=0).head(10)
+data = data.sort_values('Position')[METRICS]
 
+# generate heatmap
+fig = px.imshow(data.values,
+                labels=dict(x='Metric',y='Player'),
+                x=METRICS,
+                y=data.index)
+fig.update_xaxes(side="top", tickangle=0)
+fig.update_layout(margin = dict(t=20,r=20,b=20,l=20),
+                  width=700,
+                  height=800,
+                  autosize=False)
 
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Haikyuu!! Stats'
 server = app.server
-app.title='Haikyuu!! Stats'
+
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1('Haikyuu Player Stats'),
-    # dcc.Graph(
-    #     id='graph_id',
-    #     figure=fig_obj
-    # ),
+    html.H1('Haikyuu!! Player Stats'),
+    dcc.Graph(
+        figure=fig
+    ),
     html.A('Read about this project', href='https://angelia.substack.com/p/project-idea-haikyuu-player-stats'),
     html.Br(),
     html.A('Github', href='https://github.com/angelialau/HaikyuuPlayerStats'),
