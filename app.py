7c7976d75b37e976e22142898143a30f82219fad
angelialau <angelialkm@gmail.com>
2020-07-17 17:48:56 -0400

    Change continuous cbar to discrete, modify colors...colors are hard

--- app.py
+++ app.py
@@ -11,13 +11,23 @@ data = pd.read_csv('haikyuu_players.csv', index_col=0).head(10)
 data = data.sort_values('Position')[METRICS]
 
 # generate heatmap
-fig = go.Figure(data=go.Heatmap(z=data.values,
-                                x=METRICS,
-                                y=data.index,
-                                xgap=.05,
-                                ygap=.05,
-                                zmin=1,
-                                zmax=5))
+heatmap = go.Heatmap(z=data.values,
+                     x=METRICS,
+                     y=data.index,
+                     xgap=2,ygap=2,
+                     zmin=1,zmax=6,
+                     colorscale=[[0.0, '#445582'],[0.2, '#445582'],
+                                 [0.2, '#835699'],[0.4, '#835699'],
+                                 [0.4, '#CA4C8C'],[0.6, '#CA4C8C'],
+                                 [0.6, '#FA4F5D'],[0.8, '#FA4F5D'],
+                                 [0.8, '#FF7B00'],[1.0, '#FF7B00']
+                                 ],
+                     colorbar=dict(thickness=25,
+                                   tickvals=(1.5, 2.5, 3.5, 4.5, 5.5),
+                                   ticktext=(1,2,3,4,5),
+                                   )
+                     )
+fig = go.Figure(data=heatmap)
 fig.update_xaxes(side='top', title='Metrics')
 fig.update_yaxes(title='Player')
 
